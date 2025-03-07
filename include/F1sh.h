b311ec3dd6e9a8bf77fd8c350c94573d13d7fcc2
b4iterdev <thaimeo200389@gmail.com>
2025-03-07 16:28:56 +0700

    transfer F1sh into header file

---
+++ include/F1sh.h
@@ -0,0 +1,162 @@
+#include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+#include "AsyncJson.h"
+#include <ArduinoJson.h>
+
+typedef void (*MotorCallback)(int, int, int, int);
+typedef void (*ServoCallback)(int, int, int, int, int, int);
+
+
+static AsyncWebServer server(80);
+static AsyncWebSocket ws("/ws");
+
+static MotorCallback motorCallback = nullptr;
+static ServoCallback servoCallback = nullptr;
+
+void initWiFiAP(const char *ssid,const char *password,const char *hostname, int channel) {
+    Serial.print("\n\nCreating hotspot");
+    WiFi.setHostname(hostname);
+    WiFi.encryptionType(WIFI_AUTH_WPA2_PSK);
+    WiFi.begin(ssid, password);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid,password,(channel >= 1) && (channel <= 13) ? channel : int(random(1, 13)));
+    Serial.print("IP address: ");
+    Serial.println(WiFi.getMode() == WIFI_AP ? WiFi.softAPIP() : WiFi.localIP());
+}
+
+void initWiFiSmart() {
+    WiFi.mode(WIFI_STA);
+    WiFi.beginSmartConfig();
+    //Wait for SmartConfig packet from mobile
+    Serial.println("Waiting for SmartConfig.");
+    while (!WiFi.smartConfigDone()) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("SmartConfig received.");
+    //Wait for WiFi to connect to AP
+    Serial.println("Waiting for WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi Connected.");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.getMode() == WIFI_AP ? WiFi.softAPIP() : WiFi.localIP());
+}
+
+void initWebServer() {
+    ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+      (void)len;
+  
+      if (type == WS_EVT_CONNECT) {
+        ws.textAll("new client connected");
+        Serial.println("ws connect");
+        client->setCloseClientOnQueueFull(false);
+        client->ping();
+  
+      } else if (type == WS_EVT_DISCONNECT) {
+        ws.textAll("client disconnected");
+        Serial.println("ws disconnect");
+  
+      } else if (type == WS_EVT_ERROR) {
+        Serial.println("ws error");
+  
+      } else if (type == WS_EVT_PONG) {
+        Serial.println("ws pong");
+  
+      } else if (type == WS_EVT_DATA) {
+        AwsFrameInfo *info = (AwsFrameInfo *)arg;
+        Serial.printf("index: %" PRIu64 ", len: %" PRIu64 ", final: %" PRIu8 ", opcode: %" PRIu8 "\n", info->index, info->len, info->final, info->opcode);
+        String msg = "";
+        if (info->final && info->index == 0 && info->len == len) {
+          if (info->opcode == WS_TEXT) {
+            data[len] = 0;
+            Serial.printf("ws text: %s\n", (char *)data);
+  
+            // Parse the JSON message
+            JsonDocument doc;
+            DeserializationError error = deserializeJson(doc, data);
+            if (error) {
+              Serial.print(F("deserializeJson() failed: "));
+              Serial.println(error.f_str());
+              return;
+            }
+  
+            // Extract data
+            if (!doc["action"].isNull()) {
+              const char* action = doc["action"];
+              Serial.println(action);
+              if (doc["action"] == "motor")
+              {
+                const int c1 = doc["c1"].as<int>();
+                const int c2 = doc["c2"].as<int>();
+                const int c3 = doc["c3"].as<int>();
+                const int c4 = doc["c4"].as<int>();
+                Serial.printf("c1: %d  c2: %d  c3: %d  c4: %d\n", c1, c2, c3, c4);
+                if (motorCallback) {
+                  motorCallback(c1, c2, c3, c4);
+                }
+              }
+              if (doc["action"] == "servo")
+              {
+                const int s1 = doc["s1"].as<int>();;
+                const int s2 = doc["s2"].as<int>();;
+                const int s3 = doc["s3"].as<int>();;
+                const int s4 = doc["s4"].as<int>();;
+                const int s5 = doc["s5"].as<int>();;
+                const int s6 = doc["s6"].as<int>();;
+                Serial.printf("s1: %d  s2: %d  s3: %d  s4: %d  s5: %d  s6: %d\n",s1,s2,s3,s4,s5,s6);
+                if (servoCallback) {
+                  servoCallback(s1, s2, s3, s4, s5, s6);
+                }
+              }
+              if (doc["action"] == "reboot") {
+                ESP.restart();
+              }
+            }
+          }
+        }
+      }
+    });
+    server.rewrite("/config", "/index.html");
+    server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
+    server.addHandler(&ws);
+    server.begin();
+}
+
+void setMotorCallback(MotorCallback callback) {
+    motorCallback = callback;
+}
+
+void setServoCallback(ServoCallback callback) {
+    servoCallback = callback;
+}
+
+void F1shInitAP(const char *ssid,const char *password,const char *hostname, int channel) {
+  Serial.println("Starting F1sh as an Access Point");
+#ifdef ESP32
+  LittleFS.begin(true);
+#else
+  LittleFS.begin();
+#endif
+  initWiFiAP(ssid,password,hostname,channel);
+  initWebServer();
+}
+
+void F1shInitSmartAP(){
+  Serial.println("Starting F1sh in SmartConfig mode");
+#ifdef ESP32
+    LittleFS.begin(true);
+#else
+    LittleFS.begin();
+#endif
+    initWiFiSmart();
+    initWebServer();
+}
+
+void F1shLoop() {
+  ws.cleanupClients();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,11 @@
 #include <RPAsyncTCP.h>
 #include <WiFi.h>
 #endif
+#include <F1sh.h>
+#include <motor.h>
 
 // Remove comment from this line to use ESP Smart Config
-// #define USE_SMART_CONFIG
-
-#include <ESPAsyncWebServer.h>
-#include <LittleFS.h>
-#include "AsyncJson.h"
-#include <ArduinoJson.h>
-#include <motor.h>
+#define USE_SMART_CONFIG
 
 const char *ssid = "F1sh";
 const char *password = "stemistclub";
@@ -26,129 +22,29 @@ const char *hostname = "f1sh.local";
 
 const int channel = 0; // 1-13 - You should change this if there are multiple APs in the area.
 
-static AsyncWebServer server(80);
-static AsyncWebSocket ws("/ws");
-
-void initWiFiAP() {
-#ifdef USE_SMART_CONFIG
-  WiFi.mode(WIFI_STA);
-  WiFi.beginSmartConfig();
-  //Wait for SmartConfig packet from mobile
-  Serial.println("Waiting for SmartConfig.");
-  while (!WiFi.smartConfigDone()) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("SmartConfig received.");
-  //Wait for WiFi to connect to AP
-  Serial.println("Waiting for WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("WiFi Connected.");
-#endif
-#ifndef USE_SMART_CONFIG
-  Serial.print("\n\nCreating hotspot");
-  WiFi.setHostname(hostname);
-  WiFi.encryptionType(WIFI_AUTH_WPA2_PSK);
-  WiFi.begin(ssid, password);
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid,password,(channel >= 1) && (channel <= 13) ? channel : int(random(1, 13)));
-#endif
-  Serial.print("IP address: ");
-  Serial.println(WiFi.getMode() == WIFI_AP ? WiFi.softAPIP() : WiFi.localIP());
+void handleMotors(int c1, int c2, int c3, int c4) {
+  // Your motor control code here
+  Serial.printf("Motor callback: %d, %d, %d, %d\n", c1, c2, c3, c4);
 }
 
-void initWebServer() {
-  ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-    (void)len;
-
-    if (type == WS_EVT_CONNECT) {
-      ws.textAll("new client connected");
-      Serial.println("ws connect");
-      client->setCloseClientOnQueueFull(false);
-      client->ping();
-
-    } else if (type == WS_EVT_DISCONNECT) {
-      ws.textAll("client disconnected");
-      Serial.println("ws disconnect");
-
-    } else if (type == WS_EVT_ERROR) {
-      Serial.println("ws error");
-
-    } else if (type == WS_EVT_PONG) {
-      Serial.println("ws pong");
-
-    } else if (type == WS_EVT_DATA) {
-      AwsFrameInfo *info = (AwsFrameInfo *)arg;
-      Serial.printf("index: %" PRIu64 ", len: %" PRIu64 ", final: %" PRIu8 ", opcode: %" PRIu8 "\n", info->index, info->len, info->final, info->opcode);
-      String msg = "";
-      if (info->final && info->index == 0 && info->len == len) {
-        if (info->opcode == WS_TEXT) {
-          data[len] = 0;
-          Serial.printf("ws text: %s\n", (char *)data);
-
-          // Parse the JSON message
-          JsonDocument doc;
-          DeserializationError error = deserializeJson(doc, data);
-          if (error) {
-            Serial.print(F("deserializeJson() failed: "));
-            Serial.println(error.f_str());
-            return;
-          }
-
-          // Extract data
-          if (!doc["action"].isNull()) {
-            const char* action = doc["action"];
-            Serial.println(action);
-            if (doc["action"] == "motor")
-            {
-              const char* c1 = doc["c1"];
-              const char* c2 = doc["c2"];
-              const char* c3 = doc["c3"];
-              const char* c4 = doc["c4"];
-              Serial.printf("c1: %s  c2: %s  c3: %s  c4: %s\n",c1,c2,c3,c4);
-              setPWMMotors(int(c1),int(c2),int(c3),int(c4));
-            }
-            if (doc["action"] == "servo")
-            {
-              const char* s1 = doc["s1"];
-              const char* s2 = doc["s2"];
-              const char* s3 = doc["s3"];
-              const char* s4 = doc["s4"];
-              const char* s5 = doc["s5"];
-              const char* s6 = doc["s6"];
-              Serial.printf("s1: %s  s2: %s  s3: %s  s4: %s  s5: %s  s6: %s\n",s1,s2,s3,s4,s5,s6);
-            }
-            if (doc["action"] == "reboot") {
-              ESP.restart();
-            }
-          }
-        }
-      }
-    }
-  });
-  server.rewrite("/config", "/index.html");
-  server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
-  server.addHandler(&ws);
-  server.begin();
+void handleServos(int s1, int s2, int s3, int s4, int s5, int s6) {
+  // Your servo control code here
+  Serial.printf("Servo callback: %d, %d, %d, %d, %d, %d\n", s1, s2, s3, s4, s5, s6);
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting...");
   initMotors();
-#ifdef ESP32
-  LittleFS.begin(true);
-#else
-  LittleFS.begin();
-#endif
-  initWiFiAP();
-  initWebServer();
+  #ifdef USE_SMART_CONFIG
+  F1shInitSmartAP();
+  #else
+  F1shInitAP(ssid, password, hostname, channel);
+  #endif
+  // Register callbacks
+  setMotorCallback(handleMotors);
+  setServoCallback(handleServos);
 }
 
 void loop() {
-  ws.cleanupClients();
+  F1shLoop();
 }
