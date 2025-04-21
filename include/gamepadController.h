a5ff84a281eaac1cd0eac7d4821963f80378510c
EinSl3y <147304527+EinSl3y@users.noreply.github.com>
2025-04-21 23:10:17 +0700

    Update gamepadController.h

--- include/gamepadController.h
+++ include/gamepadController.h
@@ -1,15 +1,11 @@
 #include <Arduino.h>
 #include <F1sh.h>
-
 // Credit: https://github.com/makerviet/maker-bot
-
 #define DEBUG
-
 #define TOP_SPEED 4095
 #define NORM_SPEED 2048
 #define PRECISE_SPEED 2048
 #define TURNING_FACTOR 1
-
 #define SINGLE_HAND_DRIVING 0
 #define TWO_HAND_DRIVING 1
 
@@ -25,19 +21,29 @@ void gamepadControl(float X, float Y, float buttons[][17]){
     } else {
         speed = NORM_SPEED;
     }
-    int nJoyX = 128 - X;
+    int dZ = 10;
+    int nJoyX = X - 128;
     int nJoyY = 128 - Y;
-    int nMotMixL;                          // Motor (left) mixed output
-    int nMotMixR;                          // Motor (right) mixed output
+    if (nJoyX > dZ)
+        nJoyX = 128;
+    else if (nJoyX < -dZ)
+        nJoyX = -128;
+    else
+        nJoyX = 0;
 
+    if (nJoyY > dZ)
+        nJoyY = 128;
+    else if (nJoyY < -dZ)
+        nJoyY = -128;
+    else
+        nJoyY = 0;
+    int nMotMixL = 0;
+    int nMotMixR = 0;
     bool temp = (nJoyY * nJoyX > 0);
-    if (nJoyX) // Turning
-    {
+    if (nJoyX) {
         nMotMixL = -nJoyX + (nJoyY * temp);
         nMotMixR = nJoyX + (nJoyY * !temp);
-    }
-    else // Forward or Reverse
-    {
+    } else {
         nMotMixL = nJoyY;
         nMotMixR = nJoyY;
     }
@@ -47,31 +53,20 @@ void gamepadControl(float X, float Y, float buttons[][17]){
     Serial.print("\t");
     Serial.println(nMotMixR);
     #endif
-    c1 = 0, c2 = 0, c3 = 0, c4 = 0;
-    if (nMotMixR > 0)
-    {
-        c3 = nMotMixR;
-        c3 = map(c3, 0, 128, 0, speed);
+    c1 = c2 = c3 = c4 = 0;
+    if (nMotMixR > 0) {
+        c3 = map(nMotMixR, 0, 128, 0, speed);
+    } else if (nMotMixR < 0) {
+        c4 = map(abs(nMotMixR), 0, 128, 0, speed);
     }
-
-    else if (nMotMixR < 0)
-    {
-        c4 = abs(nMotMixR);
-        c4 = map(c4, 0, 128, 0, speed);
-    }
-
-    if (nMotMixL > 0)
-    {
-        c1 = nMotMixL;
-        c1 = map(c1, 0, 128, 0, speed);
-    }
-    else if (nMotMixL < 0)
-    {
-        c2 = abs(nMotMixL);
-        c2 = map(c2, 0, 128, 0, speed);
+    if (nMotMixL > 0) {
+        c1 = map(nMotMixL, 0, 128, 0, speed);
+    } else if (nMotMixL < 0) {
+        c2 = map(abs(nMotMixL), 0, 128, 0, speed);
     }
     setPWMMotors(1, c1);
     setPWMMotors(2, c2);
     setPWMMotors(3, c3);
     setPWMMotors(4, c4);
+    Serial.printf("c1: %d c2: %d c3: %d c4: %d\n", c1, c2, c3, c4);
 }
